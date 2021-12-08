94ff5f955714a1444fb434943210e21c270cb89d
Ayoola Nurudeen Etiko <nurudeenetiko12@gmail.com>
2021-12-07 18:33:29 -0700

    hahah

---
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ayoolanurudeenetiko/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ayoolanurudeenetiko.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
